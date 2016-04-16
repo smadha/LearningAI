fa37631514074d4291c7b4c6cfa840dbcb87d631
smadha <msharan@usc.edu>
2016-04-15 23:40:42 -0700

    working joint conditional

--- src/ai/as3/enumeration.py
+++ src/ai/as3/enumeration.py
@@ -1,19 +1,20 @@
-def enumerate_ask(target_variable, observations, bayes_net, variables):
+def enumerate_ask(target_variables, observations, bayes_net, variables):
     '''
-    target_variable = Single variable name -> NightDefense
+    target_variables = Single variable name -> NightDefense
     observations = given events -> {NightDefense : +, Infiltration : - }
     bayes_net - Dictionary of EventNode 
     '''
      
-    calc_prob = {} # required probability numbers for target_variable
-#     hidden_variables = [v for v in bayes_net.keys() if v != target_variable and v not in observations.keys()]
+    calc_prob = {} # required probability numbers for target_variables
+    
+    hidden_variables = [v for v in variables if v not in observations.keys()]
     
     # TODO check if observations could be provided for nodes with parents
         
     # TODO make xi restricted to asked values
     for xi in ["+","-"]:
-        print target_variable, xi, observations
-        calc_prob[xi] = enumerate_all(variables, extend(observations, target_variable, xi), bayes_net)
+        print target_variables, xi, observations
+        calc_prob[xi] = enumerate_all(hidden_variables, extend(observations, target_variables, xi), bayes_net)
     
     print calc_prob
     return calc_prob
@@ -23,18 +24,18 @@ def enumerate_all(variables, observations, bayes_net):
     variable = Single variable name -> NightDefense
     observations -> {'LeakIdea': '-', 'Demoralize': '+', 'Infiltration': '+'} value +/-
     '''
-    print variables, observations, "\n"
+#     print variables, observations, "\n"
     if not variables or len(variables) == 0: 
         return 1
     
     first = variables[0]; rest = variables[1:]
     
-    if not set(bayes_net[first].getParents()).issubset(observations):
-        # observation for parents are not available yet
-        # add this to rest and we can process it later
-        rest.append(first)   
-        print "Appended for re-evaluation" - first
-        return enumerate_all(rest, observations, bayes_net)
+#     if not set(bayes_net[first].getParents()).issubset(observations):
+#         # observation for parents are not available yet
+#         # add this to rest and we can process it later
+#         rest.append(first)   
+#         print "Appended for re-evaluation" - first
+#         return enumerate_all(rest, observations, bayes_net)
     
     # observation for parents are available
     obs_parents = ''.join([observations[p] for p in bayes_net[first].getParents()])
@@ -42,16 +43,16 @@ def enumerate_all(variables, observations, bayes_net):
     if first in observations:     
         prob_first = bayes_net[first].getProbability(observations[first], obs_parents)
         total_prob = prob_first * enumerate_all(rest, observations, bayes_net)
-        print first, observations[first], obs_parents, prob_first, total_prob
+#         print first, observations[first], obs_parents, prob_first, total_prob
         return total_prob 
     else :
         prob_first = []
         for value in ["+","-"]:
             prob_first_value = bayes_net[first].getProbability(value, obs_parents)
-            print first, value, obs_parents, prob_first_value
+#             print first, value, obs_parents, prob_first_value
             prob_first.append( prob_first_value * enumerate_all(rest, extend(observations, first, value), bayes_net) )
         
-        print first, obs_parents, sum(prob_first) 
+#         print first, obs_parents, sum(prob_first) 
         return sum(prob_first)
 
 def extend(event, var, val):
--- src/ai/as3/hw3cs561s16.py
+++ src/ai/as3/hw3cs561s16.py
@@ -1,5 +1,6 @@
 import sys
-from enumeration import *
+from enumeration import enumerate_ask
+from decimal import Decimal
 
 ip_file = sys.argv[2]
 print ip_file
@@ -15,14 +16,23 @@ Value - EventNode
 '''
 bayes_net = {}
 
+'''
+Utilty
+Decison
+Bayes
+Maximum
+'''
+
 
 class EventNode:
     '''
     Demoralize ['NightDefense', 'Infiltration'] { '++': 0.3, '+-': 0.6 }
     prob = 0.4 / { '++': 0.3, '+ -': 0.6 } / decision
     '''
+    variables = []
     def __init__(self, name, parents, prob):
         parents = parents.split()
+        EventNode.variables.append(name.strip())
         self.name = name
         self.parents = parents 
         self.prob = prob
@@ -99,17 +109,71 @@ with open(ip_file, 'r') as f:
                         line = f.readline().strip()
                         prob[''.join(line.split()[1:])] = float(line.split()[0])
                 else:
-                    prob = float(f.readline().strip())
+                    prob = f.readline().strip()
+                    if prob != 'decision':
+                        prob = float(prob)
                 bayes_net[name] = EventNode(name , parent, prob)
                 
         line = f.readline()
     
     print bayes_net
 
-enumerate_ask("Demoralize", {'LeakIdea' : '+', 'Infiltration' :'+'}, bayes_net,['NightDefense','Demoralize'])
+
+# enumerate_ask("Demoralize", {'LeakIdea' : '+', 'Infiltration' :'+'}, bayes_net,EventNode.variables)
+# enumerate_ask("Infiltration", {}, bayes_net,EventNode.variables)
+
+# sample 4 
+# query 2 A+ B-
+# enumerate_ask("A", {'B' : '-'}, bayes_net,EventNode.variables)
+# enumerate_ask("B", {'A' : '+'}, bayes_net,EventNode.variables)
+# P(B = + | C = -)
+# enumerate_ask("B", {'C' : '-'}, bayes_net,EventNode.variables)
+# enumerate_ask("B", {}, bayes_net,EventNode.variables)
+# enumerate_ask("C", {'B' : '+'}, bayes_net,EventNode.variables)
+# enumerate_ask("C", {}, bayes_net,EventNode.variables)
+
+output = []
+for query in query_str:
+    observations = {}
+    target_variables = {}
+    possible_case = ["P", "EU", "MEU"]
+    
+    case,query = query.split("(")
+    query = query[:-1]
+    
+    query = query.split("|")
+
+    for t_var in query[0].split(","):
+        target_variables [t_var.split("=")[0].strip()] = t_var.split("=")[1].strip()
+    if len(query) > 1:
+        for obs in query[1].split(","):
+            observations [obs.split("=")[0].strip()] = obs.split("=")[1].strip()
+    
+#     print case
+#     print target_variables
+#     print observations
+#     print query
+    result = 1.0
+    
+    for target_variable in target_variables:
+        joint_obs = target_variables.copy()
+        del(joint_obs[target_variable])
+        joint_obs.update(observations)
+#         
+        res_target_variable = enumerate_ask(target_variable, joint_obs, bayes_net,EventNode.variables)
+#         print res_target_variable[target_variables[target_variable]]
+        result = result * res_target_variable[target_variables[target_variable]]
+        
+        
+    output.append(result)
 
 
-
+with open("output.txt", "w") as o:
+    for prob in output:
+        prob = Decimal(str(prob)).quantize(Decimal('.01'))
+        o.write(str(prob) )
+        o.write("\n")
+    
 # node = EventNode('Demoralize' ,'NightDefense Infiltration',{'++': .3 , '+-':0.6 })
 # print node.hasParents()
 # print node.possibleValues()
